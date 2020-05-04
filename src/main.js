374ef598d814fed38d8892bbce7d533f77c8a2b7
Renato A P S Filho <61285566+SrRenato@users.noreply.github.com>
2020-05-04 14:59:34 -0300

    Criado repositorio para o desafio da He4rt

---
+++ src/main.js
@@ -0,0 +1,22 @@
+var btncalculo = document.getElementById("btncalcular");
+
+btncalculo.onclick = function(){
+
+    var valorProjeto = document.getElementById ("valortotal");
+    var diasEfetivos = document.getElementById ("diastrab");
+    var horasDiarias = document.getElementById ("qht");
+    var diasFerias = document.getElementById ("diasferias");
+    
+    var diasFerias = parseInt(diasferias.value);
+    var valorProjeto = parseFloat(valortotal.value);
+    var diasEfetivos = parseFloat(diastrab.value);
+    var horasDiarias = parseFloat(qht.value);
+
+    var valorHora = (valorProjeto / (diasEfetivos * 4 * horasDiarias) ) + ( ( diasFerias * diasEfetivos * horasDiarias ) )
+
+    var resultado = document.getElementById("resu1");
+
+    resultado.innerText = (`R$ ${valorHora.toFixed(2)}`);
+    
+
+}
